43a228edf24c00afb8f444ac9a200599929e3dbc
berniehuang2008 <berniehuang2008@163.com>
2024-04-26 23:57:46 +0800

    stt script and rename ai models

--- PhoenixCenter/ai/stt.py
+++ PhoenixCenter/ai/stt.py
@@ -1,58 +1,63 @@
-import sys
+import time
 
-try:
-    import sounddevice as sd
-except ImportError as e:
-    print("Please install sounddevice first. You can use")
-    print()
-    print("  pip install sounddevice")
-    print()
-    print("to install it")
-    sys.exit(-1)
-
+import sounddevice as sd
 import sherpa_ncnn
 
 
-def create_recognizer():
-    # Please replace the model files if needed.
-    # See https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/index.html
-    # for download links.
-    # MODEL_DIR = "ai/model/sherpa-ncnn-streaming-zipformer-zh-14M-2023-02-23"
-    MODEL_DIR = "ai/model/sherpa-ncnn-streaming-zipformer-20M-2023-02-17"
-    recognizer = sherpa_ncnn.Recognizer(
-        tokens=f"{MODEL_DIR}/tokens.txt",
-        encoder_param=f"{MODEL_DIR}/encoder_jit_trace-pnnx.ncnn.param",
-        encoder_bin=f"{MODEL_DIR}/encoder_jit_trace-pnnx.ncnn.bin",
-        decoder_param=f"{MODEL_DIR}/decoder_jit_trace-pnnx.ncnn.param",
-        decoder_bin=f"{MODEL_DIR}/decoder_jit_trace-pnnx.ncnn.bin",
-        joiner_param=f"{MODEL_DIR}/joiner_jit_trace-pnnx.ncnn.param",
-        joiner_bin=f"{MODEL_DIR}/joiner_jit_trace-pnnx.ncnn.bin",
-        num_threads=4,
-    )
-    return recognizer
+class Stt:
+    @staticmethod
+    def nullptr(*args, **kwargs):
+        pass
 
+    def __init__(self, model="sherpa-ncnn-en-20M") -> None:
+        self.resolver = Stt.nullptr
+        self.model = model
 
-def main():
-    print("Started! Please speak")
-    recognizer = create_recognizer()
-    sample_rate = recognizer.sample_rate
-    samples_per_read = int(0.1 * sample_rate)  # 0.1 second = 100 ms
-    last_result = ""
-    with sd.InputStream(channels=1, dtype="float32", samplerate=sample_rate) as s:
-        while True:
-            samples, _ = s.read(samples_per_read)  # a blocking read
-            samples = samples.reshape(-1)
-            recognizer.accept_waveform(sample_rate, samples)
-            result = recognizer.text
-            if last_result != result:
-                last_result = result
-                print(result)
+    def create_recognizer(self, model=None) -> sherpa_ncnn.Recognizer:
+        # Please replace the model files if needed.
+        # See https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/index.html
+        # for download links.
+        # MODEL_DIR = "ai/model/sherpa-ncnn-streaming-zipformer-zh-14M-2023-02-23"
+        MODEL_DIR = f"ai/model/{model or self.model or 'sherpa-ncnn-en-20M'}"
+        recognizer = sherpa_ncnn.Recognizer(
+            tokens=f"{MODEL_DIR}/tokens.txt",
+            encoder_param=f"{MODEL_DIR}/encoder_jit_trace-pnnx.ncnn.param",
+            encoder_bin=f"{MODEL_DIR}/encoder_jit_trace-pnnx.ncnn.bin",
+            decoder_param=f"{MODEL_DIR}/decoder_jit_trace-pnnx.ncnn.param",
+            decoder_bin=f"{MODEL_DIR}/decoder_jit_trace-pnnx.ncnn.bin",
+            joiner_param=f"{MODEL_DIR}/joiner_jit_trace-pnnx.ncnn.param",
+            joiner_bin=f"{MODEL_DIR}/joiner_jit_trace-pnnx.ncnn.bin",
+            num_threads=4,
+        )
+        return recognizer
 
+    def main(self):
+        print("Recognizer Started!")
+        recognizer = self.create_recognizer()
+        sample_rate = recognizer.sample_rate
+        samples_per_read = int(0.1 * sample_rate)  # 0.1 second = 100 ms
+
+        last_result = "<Empty>"
+        with sd.InputStream(channels=1, dtype="float32", samplerate=sample_rate) as s:
+            start_time = time.time()
+            while True:
+                samples, _ = s.read(samples_per_read)  # a blocking read
+                samples = samples.reshape(-1)
+                recognizer.accept_waveform(sample_rate, samples)
+                result = recognizer.text
+
+                if last_result != result:
+                    last_result = result
+                    start_time = time.time()
+
+                if result and last_result == result and time.time() - start_time > 0.7:
+                    # 1s of quiet
+                    recognizer.reset()
+                    start_time = time.time()
+                    self.resolver(result)
+                    self.resolver = Stt.nullptr
 
 if __name__ == "__main__":
-    devices = sd.query_devices()
-    print(devices)
-    default_input_device_idx = sd.default.device[0]
-    print(f'Use default device: {devices[default_input_device_idx]["name"]}')
-
-    main()
+    stt = Stt()
+    stt.resolver = print
+    stt.main()
