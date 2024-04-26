76ad5508ae6020f343e3107ecfaa3ac89175b5b3
berniehuang2008 <berniehuang2008@163.com>
2024-04-27 07:11:45 +0800

    stt complete

--- PhoenixCenter/ai/stt.py
+++ PhoenixCenter/ai/stt.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 import sounddevice as sd
 import sherpa_ncnn
@@ -10,8 +11,14 @@ class Stt:
         pass
 
     def __init__(self, model="sherpa-ncnn-en-20M") -> None:
-        self.resolver = Stt.nullptr
         self.model = model
+        self.resolve = Stt.nullptr
+        self.haveResolve = False
+        self._exit = False
+
+    def then(self, resolve) -> None:
+        self.resolve = resolve
+        self.haveResolve = True
 
     def create_recognizer(self, model=None) -> sherpa_ncnn.Recognizer:
         # Please replace the model files if needed.
@@ -31,33 +38,58 @@ class Stt:
         )
         return recognizer
 
-    def main(self):
-        print("Recognizer Started!")
-        recognizer = self.create_recognizer()
-        sample_rate = recognizer.sample_rate
-        samples_per_read = int(0.1 * sample_rate)  # 0.1 second = 100 ms
+    def start(self):
+        def reco():
+            print("Recognizer Started!")
+            recognizer = self.create_recognizer()
+            sample_rate = recognizer.sample_rate
+            samples_per_read = int(0.1 * sample_rate)  # 0.1 second = 100 ms
 
-        last_result = "<Empty>"
-        with sd.InputStream(channels=1, dtype="float32", samplerate=sample_rate) as s:
-            start_time = time.time()
-            while True:
-                samples, _ = s.read(samples_per_read)  # a blocking read
-                samples = samples.reshape(-1)
-                recognizer.accept_waveform(sample_rate, samples)
-                result = recognizer.text
+            last_result = ""
+            with sd.InputStream(
+                channels=1, dtype="float32", samplerate=sample_rate
+            ) as s:
+                start_time = time.time()
+                while True:
+                    if self._exit:
+                        break
 
-                if last_result != result:
-                    last_result = result
-                    start_time = time.time()
+                    while not self.haveResolve:
+                        time.sleep(0.01)
 
-                if result and last_result == result and time.time() - start_time > 0.7:
-                    # 1s of quiet
-                    recognizer.reset()
-                    start_time = time.time()
-                    self.resolver(result)
-                    self.resolver = Stt.nullptr
+                    samples, _ = s.read(samples_per_read)  # a blocking read
+                    samples = samples.reshape(-1)
+                    recognizer.accept_waveform(sample_rate, samples)
+                    result = recognizer.text
+
+                    if last_result != result:
+                        last_result = result
+                        start_time = time.time()
+
+                    if (
+                        result
+                        and last_result == result
+                        and time.time() - start_time > 0.7
+                    ):
+                        # 0.7s of quiet
+                        self.resolve(result)
+
+                        recognizer.reset()
+                        start_time = time.time()
+                        self.resolve = Stt.nullptr
+                        self.haveResolve = False
+
+        self.thread = threading.Thread(target=reco)
+        self.thread.start()
+
+    def exit(self):
+        self._exit = True
+
 
 if __name__ == "__main__":
     stt = Stt()
     stt.resolver = print
-    stt.main()
+    stt.start()
+    time.sleep(1)
+    print("Start ...")
+    stt.then(lambda x: (print(x), stt.exit()))
